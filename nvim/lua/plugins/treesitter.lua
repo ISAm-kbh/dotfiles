0b8c43d8aa697fc7ae70621190b8c5d158da5703
Kai Lamont <kl21c@my.fsu.edu>
2025-05-06 13:05:12 -0400

    Formatting support for blade & laravel

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "rust", "cpp", "python", "swift" },
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "rust", "cpp", "python", "swift",
+    "html", "php", },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
@@ -23,4 +24,14 @@ require'nvim-treesitter.configs'.setup {
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
+}
+
+local parser_config = require'nvim-treesitter.parsers'.get_parser_configs()
+parser_config.blade = {
+    install_info  = {
+        url = "https://github.com/EmranMR/tree-sitter-blade",
+        files = { "src/parser.c" },
+        branch = 'main',
+    },
+    filetype = 'blade',
 }
